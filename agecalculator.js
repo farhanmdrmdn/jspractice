2d503ff04e536528565c29eb6d28325f818b6e9b
farhanmdrmdn <73457012+farhanmdrmdn@users.noreply.github.com>
2020-11-27 14:23:37 +0800

    new practice

---
+++ agecalculator.js
@@ -0,0 +1,13 @@
+let myName = "Farhan";
+let myAge = 30;
+let presentYear = 2020;
+let futureYear = 2030;
+const diffInYear = futureYear - presentYear; // diff in both year
+//console.log(myAge)
+//console.log(diffInYear)
+
+var futureAge = myAge + diffInYear;
+//console.log(futureAge)
+
+console.log(`My name is ${myName}.`)
+console.log(`I am ${myAge} years old this years which is ${futureAge} years old in 2030 years.`);
